08db99ba5ff580a18f17e06235cd9777e26d42e6
Digital Fourteen <digitalfourteen@Digitals-Mac-mini.local>
2022-08-29 14:26:19 +0400

    first commit

--- src/config/gulpfile.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  icon: {
-    LP: "./src/assets/icons",
-    TEMPLATE: "./src/config/_icons.tpl",
-    GENERATED_SCSS: "./src/sass/theme",
-    DESTINATION: "./src/assets/fonts/icons",
-  },
-};
